295ba8e37944f577e8938aee33a9e6ac297eb448
CherryOnTop <133831037+harrrilan@users.noreply.github.com>
2025-02-13 15:30:57 -0500

    Create Q3.rs

---
+++ HW/HW2/Q3.rs
@@ -0,0 +1,30 @@
+use std::time::SystemTime; 
+
+
+fn sum_of_cube(k:u8) -> u32{
+
+    for i in 1..=k {
+
+        let sum = 0;
+
+        let cube = i ** 3;
+        let sum = cube + sum;
+    }
+    return sum;
+}
+
+
+fn main() {
+    let before = SystemTime::now(); 
+    
+
+    // replace this function
+    let result = sum_of_cube(10);
+    println!("fibonacci result: {}", result)
+
+
+    let after = SystemTime::now(); 
+    let difference = after.duration_since(before); 
+    let difference = difference.expect("Did the clock go back?"); 
+    println!("Time it took: {:?}", difference); 
+}
