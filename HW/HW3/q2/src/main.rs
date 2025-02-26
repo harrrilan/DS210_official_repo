fe3abd1acbc564706b4cbaa672568c05eb0e31fc
CherryOnTop <133831037+harrrilan@users.noreply.github.com>
2025-02-26 11:18:36 -0500

    Update main.rs

--- HW/HW3/q2/src/main.rs
+++ HW/HW3/q2/src/main.rs
@@ -1,2 +1,54 @@
-struct Polygon{param:f64}
+use std::f64::consts::PI;
 
+struct RegularPolygon {
+    sides: u32,
+    side_length: f64,
+}
+
+trait Polygon {
+    fn perimeter(&self) -> f64;
+    fn area(&self) -> f64;
+    fn radius(&self) -> f64; 
+    fn apothem(&self) -> f64;
+}
+
+impl Polygon for RegularPolygon {
+    fn perimeter(&self) -> f64 {
+        self.sides as f64 * self.side_length
+    }
+
+    fn apothem(&self) -> f64 {
+        self.side_length / (2.0 * (PI / self.sides as f64).tan())
+    }
+
+    fn radius(&self) -> f64 {
+        self.side_length / (2.0 * (PI / self.sides as f64).sin())
+    }
+
+    fn area(&self) -> f64 {
+        0.5 * self.perimeter() * self.apothem()
+    }
+}
+
+fn main() {
+    let side_counts = [4, 8, 16, 32, 64, 128, 256, 512, 2048, 65536];
+    let radii = [1.0, 5.0, 10.0];
+
+    for &radius in radii.iter() {
+        println!("\nRadius: {}", radius);
+        for &sides in side_counts.iter() {
+            
+            let side_length = 2.0 * radius * (PI / sides as f64).sin();
+            let poly = RegularPolygon { sides, side_length };
+
+            let poly_area = poly.area();
+            let circum_area = PI * radius * radius;
+            let in_area = PI * poly.apothem() * poly.apothem();
+
+            println!(
+                "Sides: {:5} | Poly Area: {:.4} | Circum Circle: {:.4} | In Circle: {:.4}",
+                sides, poly_area, circum_area, in_area
+            );
+        }
+    }
+}
